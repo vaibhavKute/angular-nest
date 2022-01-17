19f1aada7393016ca37c426e18cf319e6f491df2
vaibhavKute <kutevaibhav7@gmail.com>
2022-01-17 23:25:05 +0530

    login password visibility icon

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -19,6 +19,7 @@ export class LoginComponent implements OnInit {
   errorMsg;
   errorMessage;
   responseMessage;
+  hide = true;
 
   constructor(private fb: FormBuilder, private snackBar: MatSnackBar, private httpService: ApiServicesService, private router: Router) { }
 
